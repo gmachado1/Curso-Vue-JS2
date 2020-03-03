e90b066153870beef55dfe98a967ff974f99a29a
guaraujo <g.machado1@gmail.com>
2020-03-03 18:34:26 -0300

    finalizando secao pluguins secao 6

---
+++ vue-client/projeto-simples/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    Olá Vue {{titulo}}
+    <button @click="titulo +='####'">Alterar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  el: "#app",
+  data: function() {
+    return {
+      titulo: "Teste data usando Vue JS!!"
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  background-color: chocolate;
+  color: aliceblue;
+}
+</style>
