7a1d9cd6cda3139dff5ef171015029ea04a9243e
guaraujo <g.machado1@gmail.com>
2020-03-11 10:44:15 -0300

    finalizando compoenten avancados

---
+++ componentes-avancado/citacoes/src/components/Citacao.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="citacao">
+    {{texto}}
+    <slot name="autor"></slot>
+    <slot name="texto"></slot>
+    <slot name="fonte"></slot>
+  </div>
+</template>
+
+<script>
+import Citacoes from "./Citacoes";
+
+export default {
+  components: { Citacoes },
+  props: ["texto"],
+  destroyed() {
+    console.log("destroyed");
+  },
+  created() {
+    console.log("created");
+  },
+  activated(){
+      console.log('activated')
+  },
+  deactivated(){
+      console.log('deactivated')
+  }
+};
+</script>
+
+<style scoped>
+.citacao {
+  border: 1px solid #ddd;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 25px;
+  max-width: 450px;
+}
+</style>
