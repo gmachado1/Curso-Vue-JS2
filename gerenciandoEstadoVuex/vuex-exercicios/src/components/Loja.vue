fa9def0a94677e993119b46c9dbeedfa5535d57a
guaraujo <g.machado1@gmail.com>
2020-04-07 14:51:25 -0300

    iniciando capitulo 17

---
+++ gerenciandoEstadoVuex/vuex-exercicios/src/components/Loja.vue
@@ -0,0 +1,69 @@
+<template>
+    <Painel titulo="Loja Virtual" verde>
+        <div class="loja">
+            <span>Adicionar</span>
+            <input type="number" v-model.number="quantidade">
+            <span>itens de <strong>R$</strong></span>
+            <input type="number" v-model.number="preco">
+            <button @click="adicionar">Agora!</button>
+        </div>
+    </Painel>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+// import {mapMutations} from 'vuex'
+
+export default {
+    data() {
+        return {
+            sequencia: 1,
+            quantidade: 1,
+            preco: 9.99,
+        }
+    },
+    methods: {
+        // ...mapMutations([ 
+        //     'adicionarProduto'
+        // ]),
+        ...mapActions([ 
+            'adicionarProduto'
+        ]),
+        // O mapAction faz o que esta abaixo
+        // adicionarProduto(produto){
+        //     this.$store.dispatch('adicionarProduto',produto)
+        // },
+        adicionar() {
+            const produto = {
+                id: this.sequencia,
+                nome: `Produto ${this.sequencia}`,
+                quantidade: this.quantidade,
+                preco: this.preco
+            }
+            this.sequencia++
+            // eslint-disable-next-line
+            console.log(produto)
+            // this.$store.state.produtos.push(produto)
+            // this.$store.commit('adicionarProduto',produto)
+            //  this.adicionarProduto(produto)
+            this.$store.dispatch('adicionarProduto',produto)
+        }
+    }
+}
+</script>
+
+<style>
+    .loja {
+        display: flex;
+        justify-content: center;
+    }
+
+    .loja > * {
+        margin: 0px 10px;
+    }
+
+    input {
+        font-size: 2rem;
+        width: 90px;
+    }
+</style>
